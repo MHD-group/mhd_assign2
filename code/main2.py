03f7da4645ea2b911e44fb0dea0ef30cdaacf967
Myron <yh131996@mail.ustc.edu.cn>
2023-04-12 02:06:31 +0800

    [ADD] : experiment of problem2

--- code/main2.py
+++ code/main2.py
@@ -3,7 +3,7 @@
 # vim:fenc=utf-8
 #
 # Created On  : 2023-04-03 00:24
-# Last Modified : 2023-04-03 16:58
+# Last Modified : 2023-04-12 02:05
 # Copyright © 2023 myron <yh131996@mail.ustc.edu.cn>
 #
 # Distributed under terms of the MIT license.
@@ -52,14 +52,15 @@ def Upwind(x, C=1, t=1):
         next = (n%2 + 1)%2
         for i in range(N):
             I = i - 1
+#            tmp[next,I+1] =  tmp[cur,I+1] - C*(tmp[cur, I+1] - tmp[cur, I])
             tmp[next,I+1] =  tmp[cur,I+1] - C*0.5*(tmp[cur, I+1]**2 - tmp[cur, I]**2)
             result = tmp[next]
     return tmp[0]
 
-def minmod(a, b):
+def minabs(a, b):
     return 0 if  a * b < 0 else min([a, b]) if b > 0 else max([a, b])
 
-def limiter(x, C=1, t=1):
+def Minmod(x, C=1, t=1):
     N = x.size
     tmp = np.zeros((2, N), dtype=x.dtype)
     tmp[0] = x.copy()
@@ -70,9 +71,20 @@ def limiter(x, C=1, t=1):
         nex = (n%2 + 1)%2
         for i in range(N):
             I = i - 2
-            tmp[nex,I+1] =  tmp[cur,I+1] - C*(tmp[cur, I+1] - tmp[cur, I]) - 0.5 * C * (1 - C) *\
-			( minmod(tmp[cur, I+1]-tmp[cur, I], tmp[cur, I+2]-tmp[cur, I+1]) - \
-                        minmod(tmp[cur, I]-tmp[cur, I-1], tmp[cur, I+1]-tmp[cur, I]) )
+#            tmp[nex,I+1] =  tmp[cur,I+1] - C*(tmp[cur, I+1] - tmp[cur, I]) \
+#                    - 0.5 * C \
+#		    * ( minabs(tmp[cur, I+1]-tmp[cur, I], tmp[cur, I+2]-tmp[cur, I+1]) \
+#                    - minabs(tmp[cur, I]-tmp[cur, I-1], tmp[cur, I+1]-tmp[cur, I]) ) \
+#                    + 0.5 * C ** 2 \
+#		    * ( minabs(tmp[cur, I+1]-tmp[cur, I], tmp[cur, I+2]-tmp[cur, I+1]) \
+#                    - minabs(tmp[cur, I]-tmp[cur, I-1], tmp[cur, I+1]-tmp[cur, I]) )
+            tmp[nex,I+1] =  tmp[cur,I+1] - C*0.5*(tmp[cur, I+1]**2 - tmp[cur, I]**2) \
+                    - 0.5 * C \
+		    * 0.5 * ( minabs(tmp[cur, I+1]**2-tmp[cur, I]**2, tmp[cur, I+2]**2-tmp[cur, I+1]**2) \
+                    - minabs(tmp[cur, I]**2-tmp[cur, I-1]**2, tmp[cur, I+1]**2-tmp[cur, I]**2) ) \
+                    + 0.5 * C ** 2 \
+		    * 0.5 *( minabs(tmp[cur, I+1]**2-tmp[cur, I]**2, tmp[cur, I+2]**2-tmp[cur, I+1]**2) \
+                    - minabs(tmp[cur, I]**2-tmp[cur, I-1]**2, tmp[cur, I+1]**2-tmp[cur, I]**2) )
             result = tmp[nex]
 
     return result
@@ -84,9 +96,13 @@ if  __name__ == '__main__':
     parser.add_argument("-x", "--resolution", default=0.01, type=float, help="length of Δx")
     parser.add_argument("-C", "--ratio", default=0.5, type=float, help="Δt/Δx")
     parser.add_argument("-i", "--input", default=1, type=int, help="f(x) when t=0")
-    parser.add_argument("-m", "--method", default="Upwind", type=str, help="methods")
+    parser.add_argument("-m", "--methods", default="Upwind,Minmod", type=str, help="methods")
+    parser.add_argument("-t", "--times", default="0.5,0.75", type=str, help="time")
     args = parser.parse_args()
 
+    Ts = [float(idx) for idx in args.times.split(',')]
+    methods = args.methods.split(',')
+    print(Ts)
     # Δx: args.resolution
     x = np.arange(-1, 2, args.resolution)
     # C is Δt/Δx
@@ -94,31 +110,33 @@ if  __name__ == '__main__':
     # Δt
     t = C * args.resolution
 
-    T = 0.5
-    n_t = int(T/t)
+    fig, axs = plt.subplots(len(Ts),
+                            len(methods),
+                            figsize=(14, 6))
+    for (T, i) in zip(Ts, range(len(Ts))):
+        for (method, j) in zip(methods, range(len(methods))):
+            n_t = int(T/t)
+            # math output
+            if args.input == 1:
+                M0 = func1(x, C)
+                M1 = func1(x, C, n_t)
+            elif args.input == 2:
+                M0 = func2(x, C)
+                M1 = func2(x, C, n_t)
+            else:
+                print("error input function")
 
-    print(t, n_t)
-    # math output
-    if args.input == 1:
-        M0 = func1(x, C)
-        M1 = func1(x, C, n_t)
-    elif args.input == 2:
-        M0 = func2(x, C)
-        M1 = func2(x, C, n_t)
-    else:
-        print("error input function")
+            # simu output
+            if method == "Upwind":
+                S1 = Upwind(M0, C, n_t)
+            elif method == "Minmod":
+                S1 = Minmod(M0, C, n_t)
+            else:
+                print("error input function")
+#            axs[i].plot(x, M1, alpha=0.5)
+            axs[i][j].plot(x, S1)
+            axs[i][j].set_title("$C = " + str(C) + " t = " + str(T) + "$")
 
-    # simu output
-    if args.method == "Upwind":
-        S1 = Upwind(M0, C, n_t)
-    else:
-        print("error input function")
-    fig, axs = plt.subplots(2,
-                            1,
-                            figsize=(8, 6))
-    axs[0].plot(x, M0)
-    axs[1].plot(x, M1, alpha=0.5)
-    axs[1].plot(x, S1)
     plt.show()
 
 
--- code/run.sh
+++ code/run.sh
@@ -1,3 +1,7 @@
-python main.py -x 0.003 -i 1 -C 0.5 -m Upwind &
-python main.py -x 0.003 -i 1 -C 0.5 -m limiter &
-python main2.py -x 0.003 -i 2 -C 0.5 -m Upwind
+#python main.py -x 0.003 -i 1 -C 0.5 -m Upwind &
+#python main.py -x 0.003 -i 1 -C 0.5 -m limiter &
+#python main2.py -x 0.1 -i 1 -C 0.57 -t 0.5,0.8,1.1,1.4 -m Upwind,Minmod &
+#python main2.py -x 0.03 -i 2 -C 0.4 -t 0.5,0.6,0.7,0.8 -m Upwind,Minmod &
+#python main2.py -x 0.03 -i 2 -C 0.5 -t 0.5,0.6,0.7,0.8 -m Upwind,Minmod &
+#python main2.py -x 0.03 -i 2 -C 0.6 -t 0.5,0.6,0.7,0.8 -m Upwind,Minmod
+python main2.py -x 0.003 -i 2 -C 0.1 -t 0.5,0.75,1,1.25 -m Upwind,Minmod
